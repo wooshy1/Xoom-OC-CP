9a39d3a320187e81d958463c4e9cc28e9df7fe8e
Kenneth R. Crudup <kcrudup@lab126.com>
2012-04-23 19:59:59 -0700

    cpufreq: Separate speed target revalidate time and initial set time
    Allow speed drop after min_sample_time elapses from last time
    the current speed was last re-validated as appropriate for
    current load / input boost.
    
    Allow speed bump after min_sample_time (or above_hispeed_delay)
    elapses from the time the current speed was originally set.
    
    Change-Id: Ic25687a7a53d25e6544c30c47d7ab6f27a47bee8
    Signed-off-by: Todd Poynor <toddpoynor@google.com>
    
    Conflicts:
    
    	drivers/cpufreq/cpufreq_interactive.c

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -41,6 +41,8 @@ struct cpufreq_interactive_cpuinfo {
 	int idling;
 	u64 target_set_time;
 	u64 target_set_time_in_idle;
+	u64 target_validate_time;
+	u64 target_validate_time_in_idle;
 	struct cpufreq_policy *policy;
 	struct cpufreq_frequency_table *freq_table;
 	unsigned int target_freq;
@@ -201,23 +203,27 @@ static void cpufreq_interactive_timer(unsigned long data)
 
 	new_freq = pcpu->freq_table[index].frequency;
 
-
 	/*
 	 * Do not scale down unless we have been at this frequency for the
-	 * minimum sample time.
+	 * minimum sample time since last validated.
 	 */
 	if (new_freq < pcpu->target_freq) {
-		if (cputime64_sub(pcpu->timer_run_time, pcpu->target_set_time)
-		    < min_sample_time)
+		if (cputime64_sub(pcpu->timer_run_time,
+				  pcpu->target_validate_time)
+		    < min_sample_time) {
 			goto rearm;
+		}
 	}
 
-	pcpu->target_set_time_in_idle = now_idle;
-	pcpu->target_set_time = pcpu->timer_run_time;
+	pcpu->target_validate_time_in_idle = now_idle;
+	pcpu->target_validate_time = pcpu->timer_run_time;
 
 	if (pcpu->target_freq == new_freq) {
 		goto rearm_if_notmax;
 	}
+
+	pcpu->target_set_time_in_idle = now_idle;
+	pcpu->target_set_time = pcpu->timer_run_time;
 
 	if (new_freq < pcpu->target_freq) {
 		pcpu->target_freq = new_freq;
@@ -600,6 +606,10 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 			pcpu->target_set_time_in_idle =
 				get_cpu_idle_time_us(j,
 					     &pcpu->target_set_time);
+			pcpu->target_validate_time =
+				pcpu->target_set_time;
+			pcpu->target_validate_time_in_idle =
+				pcpu->target_set_time_in_idle;
 			pcpu->governor_enabled = 1;
 			smp_wmb();
 		}
