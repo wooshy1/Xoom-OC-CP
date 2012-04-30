2026ad367080cf632563ed1be7471f67dfbd3593
Kenneth R. Crudup <kenny@panix.com>
2012-04-30 05:18:49 -0700

    mmc: add MMC_QUIRK_NONSTD_FUNC_IF
    Introduce MMC_QUIRK_NONSTD_FUNC_IF to ignore the "SDIO Standard Function
    interface code" as indicated by the card's FBR, and instead treat all
    functions as non-standard interfaces.
    
    This is required to prevent standard drivers from facing
    errors when trying to communicate with SDIO cards that erroneously
    indicate standard function interface codes.
    
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    (cherry picked from commit eab4068795d670b065164096805cbf15a19e9690)
    
    Conflicts:
    
    	include/linux/mmc/card.h

--- include/linux/mmc/card.h
+++ include/linux/mmc/card.h
@@ -129,7 +129,8 @@ struct mmc_card {
 #define MMC_QUIRK_NONSTD_SDIO	(1<<2)		/* non-standard SDIO card attached */
 						/* (missing CIA registers) */
 #define MMC_QUIRK_BROKEN_CLK_GATING (1<<3)	/* clock gating the sdio bus will make card fail */
-#define MMC_QUIRK_INAND_CMD38	(1<<4)		/* iNAND devices have broken CMD38 */
+#define MMC_QUIRK_INAND_CMD38	(1<<14)		/* iNAND devices have broken CMD38 */
+#define MMC_QUIRK_NONSTD_FUNC_IF (1<<4)		/* SDIO card has nonstd function interfaces */
 
 	unsigned int		erase_size;	/* erase size in sectors */
  	unsigned int		erase_shift;	/* if erase unit is power 2 */
@@ -269,6 +270,11 @@ static inline int mmc_card_lenient_fn0(const struct mmc_card *c)
 static inline int mmc_blksz_for_byte_mode(const struct mmc_card *c)
 {
 	return c->quirks & MMC_QUIRK_BLKSZ_FOR_BYTE_MODE;
+}
+
+static inline int mmc_card_nonstd_func_interface(const struct mmc_card *c)
+{
+	return c->quirks & MMC_QUIRK_NONSTD_FUNC_IF;
 }
 
 #define mmc_card_name(c)	((c)->cid.prod_name)
