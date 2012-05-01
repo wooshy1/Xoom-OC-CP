bdb970982c21fb413133d3b06159a75c529188dd
Kenneth R. Crudup <kenny@panix.com>
2012-05-01 05:26:43 -0700

    mmc: sdhci: Add pre and post reset processing for chip specific reset
    Marvell pxa controllers have private registers that may need to be
    modified before and after a reset is done.
    
    For example, the SD reset operation, RESET_ALL, will reset the private
    registers to their default state.  This will cause the clock adjustment
    registers that may have been programmed to have incorrect values.
    
    RESET_DATA sometimes needs to be delayed before the reset is done
    (depending on SoC) to enable any transactions being handled by the
    SDIO card to be completed.  Needed in pre SD 3.0 silicon to handle
    clock gating.
    
    Implement hooks to allow this to happen.
    
    Signed-off-by: Philip Rakity <prakity@marvell.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    (cherry picked from commit 393c1a34dd852dbf0c409ec94dd48057100480ea)
    
    Conflicts:
    
    	drivers/mmc/host/sdhci.h

--- drivers/mmc/host/sdhci.h
+++ drivers/mmc/host/sdhci.h
@@ -229,6 +229,8 @@ struct sdhci_ops {
 	void (*platform_send_init_74_clocks)(struct sdhci_host *host,
 					     u8 power_mode);
 	unsigned int    (*get_ro)(struct sdhci_host *host);
+	void	(*platform_reset_enter)(struct sdhci_host *host, u8 mask);
+	void	(*platform_reset_exit)(struct sdhci_host *host, u8 mask);
 
 	int		(*suspend)(struct sdhci_host *host, pm_message_t state);
 	int		(*resume)(struct sdhci_host *host);
