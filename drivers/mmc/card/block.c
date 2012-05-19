3bf81bed065ff2cae4f9311f063459837b9a4604
Kenneth R. Crudup <kcrudup@lab126.com>
2012-05-18 19:25:19 -0700

    mmc: block: Remove use of mmc_blk_set_blksize
    According to the specifications for SD and (e)MMC default
    blocksize (named BLOCKLEN in Spec.) must always be 512
    bytes. Since we hardcoded to always use 512 bytes, we do
    not explicitly have to set it. Future improvements should
    potentially make it possible to use a greater blocksize
    than 512 bytes, but until then let's skip this.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@stericsson.com>
    Reviewed-by: Subhash Jadavani <subhashj@codeauora.org>
    Signed-off-by: Chris Ball <cjb@laptop.org>
    (cherry picked from commit 5865f2876baa5c68fd0d50029dd220ce19f3d2af)
    
    Conflicts:
    
    	drivers/mmc/card/block.c

--- drivers/mmc/card/block.c
+++ drivers/mmc/card/block.c
@@ -1494,24 +1494,6 @@ static int mmc_blk_alloc_parts(struct mmc_card *card, struct mmc_blk_data *md)
 	return ret;
 }
 
-static int
-mmc_blk_set_blksize(struct mmc_blk_data *md, struct mmc_card *card)
-{
-	int err;
-
-	mmc_claim_host(card->host);
-	err = mmc_set_blocklen(card, 512);
-	mmc_release_host(card->host);
-
-	if (err) {
-		printk(KERN_ERR "%s: unable to set block size to 512: %d\n",
-			md->disk->disk_name, err);
-		return -EINVAL;
-	}
-
-	return 0;
-}
-
 static void mmc_blk_remove_req(struct mmc_blk_data *md)
 {
 	if (md) {
@@ -1610,7 +1592,6 @@ static const struct mmc_fixup blk_fixups[] =
 static int mmc_blk_probe(struct mmc_card *card)
 {
 	struct mmc_blk_data *md, *part_md;
-	int err;
 	char cap_str[10];
 
 	/*
@@ -1622,10 +1603,6 @@ static int mmc_blk_probe(struct mmc_card *card)
 	md = mmc_blk_alloc(card);
 	if (IS_ERR(md))
 		return PTR_ERR(md);
-
-	err = mmc_blk_set_blksize(md, card);
-	if (err)
-		goto out;
 
 	string_get_size((u64)get_capacity(md->disk) << 9, STRING_UNITS_2,
 			cap_str, sizeof(cap_str));
@@ -1654,7 +1631,7 @@ static int mmc_blk_probe(struct mmc_card *card)
  out:
 	mmc_blk_remove_parts(card, md);
 	mmc_blk_remove_req(md);
-	return err;
+	return 0;
 }
 
 static void mmc_blk_remove(struct mmc_card *card)
@@ -1693,10 +1670,6 @@ static int mmc_blk_resume(struct mmc_card *card)
 	struct mmc_blk_data *md = mmc_get_drvdata(card);
 
 	if (md) {
-#ifndef CONFIG_MMC_BLOCK_DEFERRED_RESUME
-		mmc_blk_set_blksize(md, card);
-#endif
-
 		/*
 		 * Resume involves the card going into idle state,
 		 * so current partition is always the main one.
